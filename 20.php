4dd71579adac26b2cbde144e659c3033ff17bfcf
KADEEJA SAHLA <99816026+KadeejaSahlaPV@users.noreply.github.com>
2023-10-26 10:37:58 +0530

    Update 20.php

--- 20.php
+++ 20.php
@@ -15,3 +15,49 @@
     </form>
 </body>
           </html>
+<?php
+// Establish a database connection (replace with your database credentials)
+$servername = "your_server";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get form data
+$accno = $_POST['accno'];
+$amount = $_POST['amount'];
+$transaction_type = $_POST['transaction_type'];
+
+// Retrieve the current balance for the account
+$sql = "SELECT balance FROM bank WHERE accno = $accno";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $current_balance = $row['balance'];
+
+    // Update the balance based on the transaction type
+    if ($transaction_type === "deposit") {
+        $new_balance = $current_balance + $amount;
+    } else if ($transaction_type === "withdraw") {
+        $new_balance = $current_balance - $amount;
+    }
+
+    // Update the balance in the database
+    $update_sql = "UPDATE bank SET balance = $new_balance WHERE accno = $accno";
+    if ($conn->query($update_sql) === TRUE) {
+        echo "Transaction completed successfully. New balance: $new_balance";
+    } else {
+        echo "Error updating balance: " . $conn->error;
+    }
+} else {
+    echo "Account not found.";
+}
+
+$conn->close();
+?>
